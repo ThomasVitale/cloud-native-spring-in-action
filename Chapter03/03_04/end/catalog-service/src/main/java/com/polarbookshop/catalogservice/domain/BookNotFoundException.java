21b07837024811b8f9a7676a705672a5a85c2086
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-01-17 22:46:24 +0100

    Remove old folder structure for ch03

--- Chapter03/03_04/end/catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.catalogservice.domain;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String isbn) {
-        super("The book with ISBN " + isbn + " was not found.");
-    }
-}
