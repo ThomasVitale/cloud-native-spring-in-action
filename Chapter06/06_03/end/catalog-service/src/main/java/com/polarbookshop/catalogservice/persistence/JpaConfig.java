2dbc5e1f452d59e68f8c93d1063a1ad862c849f8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-02-14 21:44:24 +0100

    Clean old ch06 version

--- Chapter06/06_03/end/catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.polarbookshop.catalogservice.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
