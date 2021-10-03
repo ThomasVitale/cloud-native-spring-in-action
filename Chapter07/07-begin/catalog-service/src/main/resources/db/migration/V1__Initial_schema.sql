b14484f219a56d4f1b3a9b5fdc22c6e0808ea301
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-10-03 23:19:56 +0200

    Update chapter 7

--- Chapter07/07-begin/catalog-service/src/main/resources/db/migration/V1__Initial_schema.sql
+++ Chapter07/07-begin/catalog-service/src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS book (
+CREATE TABLE book (
         id                  BIGSERIAL PRIMARY KEY NOT NULL,
-        created_date        timestamp NOT NULL,
-        last_modified_date  timestamp NOT NULL,
-        version             integer NOT NULL,
         author              varchar(255) NOT NULL,
         isbn                varchar(255) UNIQUE NOT NULL,
         price               float8 NOT NULL,
-        title               varchar(255) NOT NULL
+        title               varchar(255) NOT NULL,
+        created_date        timestamp NOT NULL,
+        last_modified_date  timestamp NOT NULL,
+        version             integer NOT NULL
 );
--- Chapter07/07-begin/config-service/README.md
+++ Chapter07/07-begin/config-service/README.md
@@ -15,19 +15,3 @@ After building the application, you can also run it from the Java CLI:
 ```bash
 java -jar build/libs/config-service-0.0.1-SNAPSHOT.jar
 ```
-
-## Container tasks
-
-Run Config Service as a container
-
-```bash
-docker run --name config-service -p 8888:8888 polarbookshop/config-service:0.0.1-SNAPSHOT
-```
-
-### Container Commands
-
-| Docker Command	              | Description       |
-|:-------------------------------:|:-----------------:|
-| `docker stop config-service`   | Stop container.   |
-| `docker start config-service`  | Start container.  |
-| `docker remove config-service` | Remove container. |
