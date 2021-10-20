8a10a4fca9e413f179617823d444c0108fa690f5
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-10-20 22:09:22 +0200

    Update

--- Chapter16/16-end/quote-function/src/main/java/com/polarbookshop/quotefunction/domain/Quote.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.quotefunction.domain;
-
-public record Quote (
-	String content,
-	String author,
-	Genre genre
-){}
--- Chapter16/16-end/quote-service/src/main/java/com/polarbookshop/quoteservice/domain/Quote.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.quoteservice.domain;
-
-public record Quote (
-	String content,
-	String author,
-	Genre genre
-){}
