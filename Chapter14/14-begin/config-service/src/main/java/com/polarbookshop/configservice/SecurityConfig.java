c44066c4ae5097ee52822a6acfb29f6fe7fe7f57
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-11-22 19:10:24 +0100

    Fix import

--- Chapter14/14-begin/config-service/src/main/java/com/polarbookshop/configservice/SecurityConfig.java
+++ Chapter14/14-begin/config-service/src/main/java/com/polarbookshop/configservice/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
