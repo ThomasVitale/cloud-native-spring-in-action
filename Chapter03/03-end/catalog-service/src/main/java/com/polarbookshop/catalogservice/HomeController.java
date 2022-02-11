200ba33971dc71b63bc9a85e5d12083fa8cc2fd7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-02-11 21:41:50 +0100

    Update chapter 3

--- Chapter03/03-end/catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ Chapter03/03-end/catalog-service/src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,4 +1,4 @@
-package com.polarbookshop.catalogservice.web;
+package com.polarbookshop.catalogservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
