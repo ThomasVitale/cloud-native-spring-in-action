30055d91897f5b5d39086b39dcaaf44192af1714
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-03-06 09:14:52 +0100

    Update chapter 10

--- Chapter10/10-end/order-service/src/main/java/com/polarbookshop/orderservice/order/event/OrderFunctions.java
+++ Chapter10/10-end/order-service/src/main/java/com/polarbookshop/orderservice/order/event/OrderFunctions.java
@@ -3,9 +3,9 @@ package com.polarbookshop.orderservice.order.event;
 import java.util.function.Consumer;
 
 import com.polarbookshop.orderservice.order.domain.OrderService;
-import com.polarbookshop.orderservice.order.domain.OrderStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +16,10 @@ public class OrderFunctions {
 	private static final Logger log = LoggerFactory.getLogger(OrderFunctions.class);
 
 	@Bean
-	public Consumer<OrderDispatchedMessage> dispatchOrder(OrderService orderService) {
-		return orderDispatchedMessage -> {
-			log.info("The order with id {} has been dispatched", orderDispatchedMessage.orderId());
-			orderService.updateOrderStatus(orderDispatchedMessage.orderId(), OrderStatus.DISPATCHED);
-		};
+	public Consumer<Flux<OrderDispatchedMessage>> dispatchOrder(OrderService orderService) {
+		return flux -> orderService.updateDispatchedOrder(flux)
+				.doOnNext(order -> log.info("The order with id {} has been dispatched", order.id()))
+				.subscribe();
 	}
 
 }
-
