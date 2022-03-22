e78cbc0ca8c8c77969978b0f5413b9754934c1ed
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-03-22 17:20:50 +0100

    Update chapter 16

--- Chapter16/16-end/quote-function/src/main/java/com/polarbookshop/quotefunction/QuoteFunctionApplication.java
+++ Chapter16/16-end/quote-function/src/main/java/com/polarbookshop/quotefunction/QuoteFunctionApplication.java
@@ -1,16 +1,9 @@
 package com.polarbookshop.quotefunction;
 
-import com.polarbookshop.quotefunction.domain.Quote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.TypeAccess;
-import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
-@TypeHint(
-		types = Quote.class,
-		access = TypeAccess.DECLARED_METHODS
-)
 public class QuoteFunctionApplication {
 
 	public static void main(String[] args) {
