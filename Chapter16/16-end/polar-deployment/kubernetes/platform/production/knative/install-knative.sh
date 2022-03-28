16226953030a8fd4508ec0ecd2f8659d6b25854b
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-03-28 19:23:05 +0200

    Update chapter 16

--- Chapter16/16-end/polar-deployment/kubernetes/platform/production/knative/install-knative.sh
+++ Chapter16/16-end/polar-deployment/kubernetes/platform/production/knative/install-knative.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 
 echo "\n📦 Installing Knative CRDs..."
 
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.1.0/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.3.0/serving-crds.yaml
 
 echo "\n📦 Installing Knative Serving..."
 
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.1.0/serving-core.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.3.0/serving-core.yaml
 
 echo "\n📦 Installing Kourier Ingress..."
 
-kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.1.0/kourier.yaml
+kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.3.0/kourier.yaml
 
 kubectl patch configmap/config-network \
   --namespace knative-serving \
@@ -21,6 +21,11 @@ kubectl patch configmap/config-network \
 
 echo "\n📦 Configuring DNS..."
 
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.1.0/serving-default-domain.yaml
+kubectl patch configmap/config-domain \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"127.0.0.1.sslip.io":""}}'
+
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.3.0/serving-default-domain.yaml
 
 echo "\n✅ Knative successfully installed!\n"
