d9ad6f1671093f5e76be7611e202d4ac2ccd82f7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-07-31 23:38:42 +0200

    Update chapter 9

--- Chapter09/09-begin/order-service/src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
+++ Chapter09/09-begin/order-service/src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -3,7 +3,6 @@ package com.polarbookshop.orderservice.order.domain;
 import com.polarbookshop.orderservice.config.DataConfig;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.PostgreSQLR2DBCDatabaseContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -21,7 +20,7 @@ import org.springframework.test.context.DynamicPropertySource;
 class OrderRepositoryR2dbcTests {
 
     @Container
-    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.3"));
+    static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
 
     @Autowired
     private OrderRepository orderRepository;
