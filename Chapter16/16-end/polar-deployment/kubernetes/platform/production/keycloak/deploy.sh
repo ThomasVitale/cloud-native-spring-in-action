b83bceda2f64bf90f948179add5196ecd6d7f1f7
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-12-03 23:37:46 +0100

    Update dependencies
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- Chapter16/16-end/polar-deployment/kubernetes/platform/production/keycloak/deploy.sh
+++ Chapter16/16-end/polar-deployment/kubernetes/platform/production/keycloak/deploy.sh
@@ -17,7 +17,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade --install polar-keycloak bitnami/keycloak \
   --values values.yml \
-  --namespace keycloak-system --version 10.1.8
+  --namespace keycloak-system --version 17.3.0
 
 echo "\n⌛ Waiting for Keycloak to be deployed..."
 
